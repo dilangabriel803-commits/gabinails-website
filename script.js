b20a9696b6190cef7ef24e682e1d908d39d1b16d
dilangabriel803-commits <dilangabriel803@gmail.com>
2026-07-11 01:59:51 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("Gabi Nails by Lu website loaded 💅");
+
+
+document.querySelectorAll(".button").forEach(button => {
+
+button.addEventListener("click", function(){
+
+console.log("Button clicked");
+
+});
+
+});
